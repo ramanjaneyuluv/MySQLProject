a4a86de381830c72607b47f772d2c4bdea747297
unknown <sv.reshuram@gmail.com>
2019-02-11 22:15:41 +0530

    Adding new class

---
+++ src/MySQLConnecton/GitClassDemo.java
@@ -0,0 +1,13 @@
+package MySQLConnecton;
+
+public class GitClassDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo git pusing one class");
+
+	}
+
+	public void m1(){
+		System.out.println("adding one method");
+	}
+}
